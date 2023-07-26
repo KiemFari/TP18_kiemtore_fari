dcb291773dc8152f4fc213ad3e3f7811b08c340b
Kiemtore_Fariratou <kiemtorefariratou@.com>
2023-07-26 19:20:51 +0100

    master

---
+++ index.js
@@ -0,0 +1,8 @@
+/*<div class="logo">
+                <i class="fas fa-rocket"></i>
+            </div>
+            <div class="toggle">
+                <i class="fas fa-bars ouvrir"></i>
+                <i class="fas fa-times fermer"></i>
+            </div>
+            <div class="leftside"></div>*/

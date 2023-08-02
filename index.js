20efde24ecca3060815f32aa404d82f7a9d2eee2
Kiemtore_Fariratou <kiemtorefariratou@.com>
2023-08-02 18:08:26 +0100

    master

--- index.js
+++ index.js
@@ -1,14 +1,6 @@
-/*<div class="logo">
-                <i class="fas fa-rocket"></i>
-            </div>
-            <div class="toggle">
-                <i class="fas fa-bars ouvrir"></i>
-                <i class="fas fa-times fermer"></i>
-            </div>
-            <div class="leftside"></div>*/
-            let toogle = document.querySelector(".toogle");
-            let body = document.querySelector("body");
+ let toogle = document.querySelector(".toogle");
+let body = document.querySelector("body");
 
-            toogle.addEventListener("click", function(){
-                body.classList.toggle("open")
-            });
+ toogle.addEventListener("click", function(){
+     body.classList.toggle("open")
+ });
